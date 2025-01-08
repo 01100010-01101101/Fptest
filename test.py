fbb4f42f5102f663613cc7b958daaa87ce36c6f0
01100010-01101101 <marc.alvial@duocuc.cl>
2025-01-07 21:20:23 -0300

    ejercicios varios de If-Else

--- test.py
+++ test.py
@@ -1,1 +1,106 @@
-print ("hola mundo")
+
+# nombre = "Marco Alvial"
+# edad = 29
+
+
+
+# print("Mi nombre es",nombre, "y mi edad es ",edad)
+
+
+# num1 = 0
+# num2 = 0
+
+# print("ingresa un numero")
+# num1 = int(input())
+
+# print("ingresa otro numero")
+# num2 = int(input())
+
+# print("el resultado es ",num1+num2)
+
+
+
+# print("ingresa un numero")
+# num1 = int(input())
+# if num1>0: 
+#     print("el numero es positivo")
+# else:
+#     print("el numero es negativo o cero")
+    
+    
+
+# print("ingresa tu edad")
+# edad = int(input())
+
+# if edad > 18:
+#     print("eres mayor de edad")
+# else:
+#     print("eres menor de edad")
+    
+    
+    
+# print(" ingresa 3 numeros")    
+# num1 = int(input())
+# num2 = int(input())
+# num3 = int(input())
+
+# print("el promedio es", (num1+num2+num3)/3)
+
+# Parque nacional tiene los siguientes valores
+
+# menor de 12 $500
+# entre 13 y 18 $1000
+# entre 19 y 64 $2000
+# mas de 65 $700
+
+# bonus, cuantas entradas son?
+
+
+# print("ingresa tu edad")
+# edad = int(input())
+# if edad < 12:
+#     monto=500
+#     cantidad=int(input())
+#     t=monto * cantidad
+#     print("la entrada tiene un valor de",t)
+# elif edad <= 13 and 18:
+#     monto=1000
+#     cantidad=int(input())
+#     t=monto * cantidad
+#     print("la entrada tiene un valor de",t)
+# elif edad >= 19 and  edad <= 64:
+#             monto= 2000
+#             cantidad=int(input())
+#             t=monto * cantidad
+#             print("la entrada tiene un valor de",t)
+# elif edad > 65:
+#             monto=700
+#             cantidad=int(input())
+#             t=monto * cantidad
+#             print("la entrada tiene un valor de",t)
+# else:
+#     print("el total es")
+    
+
+                
+
+
+# pertenece a la florida?
+# no = gracias por visitar
+# si = continua a la sigt
+
+# tiene carnet de socio?
+# no= creque su carnet aca(crea carnet)
+# si = ingrese su numero
+
+# es jubilado?
+# no =  pague monto total.
+# si= paga monto con -25%
+
+# su total a pagar es {total}
+
+
+# print("bienvenido al registro civil, vive en la Florida")
+# input()
+
+# if 
