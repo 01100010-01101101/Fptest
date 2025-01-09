ac0f4fa1405cd3a5c506384d24386d6d1d4191dd
01100010-01101101 <marc.alvial@duocuc.cl>
2025-01-08 21:48:40 -0300

    ejercicios de For

---
+++ For.py
@@ -0,0 +1,99 @@
+
+
+
+# for i in range(3):              3 repeticion de 0 a 3
+#     print("saludo", i)
+
+# for i in range(3):                #repeticion de 1 en 1
+#     print("saludo", i + 1)
+    
+# total = 0    
+# for i in range(2):
+#     print("ingrese gasto")
+#     gasto = int(input())    
+#     total = total + gasto
+# print(total)    
+    
+# num = 0                                   
+    
+# for num in range(0,11):
+#     for i in range(0, 10):
+#         print(num ,"x", i + 1 , " = " , (i + 1 )*num)   
+        
+        
+
+
+# for i in range(30):
+#     print("tu tienes")
+             
+             
+             
+# promedio = 0                                 # promedio de notas
+# for i in range(3):
+#     print("ingrese una nota")
+#     nota = int(input())    
+#     promedio = promedio + nota
+# print("su promedio es", promedio/(i+1))  
+                 
+                 
+                 
+# num = 2                          #programa para ver numeros pares
+
+# if num%2==0:
+#     print("el numero es par")                 
+# else:
+#     print("el numero no es par")
+    
+    
+    
+# i = int(input("ingrese un numero"))  ## programa para ver numero impares
+
+# for i in range(i):
+#     if i % 2  !=0:
+#         print(i)
+    
+          
+          
+# palabra = "hola"          # ver largo una frase 
+
+# print(len(palabra))
+
+# palabra = "chile"             #ver palabra en vertical
+# for i in palabra:
+#     print(i)
+
+
+# nombre = input()                         #programa para contar el numero de palabras en total
+# total = 0
+# for i in range(len(nombre)):
+#     total = total + (i + 1)
+    
+# print("el total de la suma de sus numeros es", total)    
+
+
+
+#secuencia fibonacci
+# def fibonacci(n):                       
+#     if n < 2:
+#         return n 
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+    
+# for i in range(20):
+#     print(fibonacci(i))    
+
+
+
+
+# for i in range(3):
+#     clave = input("ingrese la clave: ")
+#     if clave == "1234":
+#         print("clave correcta")
+#         break
+#     else:
+#         print("la clave es incorrecta")
+#         print("la pc se autodestruira")        
+    
+    
+    
+    
