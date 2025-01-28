7d7b57c56573b4ddc8501aef6ec6ef03c22b9542
01100010-01101101 <marc.alvial@duocuc.cl>
2025-01-28 19:29:51 -0300

    ejercicios listas y diccionarios

---
+++ ejercicios_matrices.py
@@ -0,0 +1,24 @@
+# crear un programa para ingresar nombre y edad de una persona,
+# para dejar de poner nombres poner 'x', luego mostrar todos los nombres,
+# con sus respectivas edades.
+
+
+nombres = []
+edades = []
+
+
+while True:
+    nombre = input("ingresa tu nombre, o pulsa 'x' para salir.")
+    if nombre == 'x':
+        break
+    edad = input("ingresa la edad: ")
+    nombres.append(nombre)
+    edades.append(edad)
+    
+print("lista de nombres y edades: ")
+for i in range(len(nombres)):
+    print(nombres[i],"-",edades[i],"años")    
+    
+    
+
+
